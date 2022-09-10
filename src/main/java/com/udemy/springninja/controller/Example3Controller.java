39996d252b45d5c39aa827f91e6e3387b70ca6c1
Daniel-DLR-02 <danidlr02@gmail.com>
2022-09-10 16:38:31 +0200

    validation

--- src/main/java/com/udemy/springninja/controller/Example3Controller.java
+++ src/main/java/com/udemy/springninja/controller/Example3Controller.java
@@ -5,12 +5,15 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/example3")
@@ -34,20 +37,24 @@ public class Example3Controller {
 
     @GetMapping("/showform")
     public String showForm(Model model){
-        LOGGER.info("INFO TRACE");
-        LOGGER.warn("WARNING TRACE");
-        LOGGER.error("ERROR TRACE");
-        LOGGER.debug("DEBUG TRACE");
         model.addAttribute("person", new Person());
         return FORM_VIEW;
     }
 
     @PostMapping("/addperson")
-    public ModelAndView addPerson(@ModelAttribute("person") Person person){
-        LOGGER.info("METHOD: 'addPerson -- PARAMS: " + person + "'");
-        ModelAndView mav = new ModelAndView(RESULT_VIEW);
-        mav.addObject("person",person);
-        LOGGER.info("TEMPLATE: '" + RESULT_VIEW + "' -- DATA '" + person + "'");
+    public ModelAndView addPerson(@Valid @ModelAttribute("person") Person person, BindingResult bindingResult){
+
+        ModelAndView mav = new ModelAndView();
+
+        // Pareciera qie binding results esta deprecado, no funciona correctamente
+        if(bindingResult.hasErrors()){
+            mav.setViewName(FORM_VIEW);
+        }else{
+            mav.setViewName(RESULT_VIEW);
+            mav.addObject("person",person);
+        }
         return mav;
+
+
     }
 }
