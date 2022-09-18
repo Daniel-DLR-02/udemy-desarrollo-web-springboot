4772384d001651378c76548bc5f5e5973378fbeb
Daniel-DLR-02 <danidlr02@gmail.com>
2022-09-18 20:39:43 +0200

    Empiezo proyecto final del curso

---
+++ src/main/java/com/udemy/springninja/controller/LoginController.java
@@ -0,0 +1,40 @@
+package com.udemy.springninja.controller;
+
+import com.udemy.springninja.model.UserCredential;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String redirectToLogin(){
+        return "redirect:/login";
+    }
+
+
+    @GetMapping("/login")
+    public String showLoginForm(Model model,
+                                @RequestParam(name="error",required = false) String error,
+                                @RequestParam(name="logout",required = false) String logout){
+        model.addAttribute("error", error);
+        model.addAttribute("logout", logout);
+        model.addAttribute("userCredentials", new UserCredential());
+        return "login";
+    }
+
+    @PostMapping("/logincheck")
+    public String loginCheck(@ModelAttribute(name="userCredential") UserCredential userCredential){
+        if(userCredential.getUsername().equals("user") && userCredential.getPassword().equals("user")){
+            return "contacts";
+        }
+        else{
+            return "redirect:/login?error";
+        }
+    }
+
+}
--- src/main/java/com/udemy/springninja/service/Ejercicio1Service.java
+++
@@ -1,5 +0,0 @@
-package com.udemy.springninja.service;
-
-public interface Ejercicio1Service {
-    public abstract void showLog();
-}
