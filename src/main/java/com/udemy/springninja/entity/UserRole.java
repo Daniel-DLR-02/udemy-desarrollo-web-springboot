86081fd58ed0cc8ff3404f379ec8284c2c487430
Daniel-DLR-02 <danidlr02@gmail.com>
2022-09-19 15:22:15 +0200

    Added user entity

---
+++ src/main/java/com/udemy/springninja/entity/UserRole.java
@@ -0,0 +1,53 @@
+package com.udemy.springninja.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="user_roles",uniqueConstraints = @UniqueConstraint(
+        columnNames = {"role","username"}
+))
+public class UserRole {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "user_role_id", unique = true, nullable = false)
+    private Long userRoleId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "username", nullable = false)
+    private User user;
+
+    @Column(name="role",nullable = false,length = 45)
+    private String role;
+
+    public UserRole(User user, String role) {
+        this.user = user;
+        this.role = role;
+    }
+
+    public UserRole() {}
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(Long userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
